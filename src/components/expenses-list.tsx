bcf2531970ffbf6aa58d6f32d6995d479f820c02
Oleh Kliapko <goldclub1977@gmail.com>
2025-02-09 14:56:56 +0200

    ADD: dashboard & its components

---
+++ src/components/expenses-list.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+const expenses = [
+  { id: 1, description: "Rent", amount: 1000 },
+  { id: 2, description: "Groceries", amount: 100 },
+  { id: 3, description: "Gas", amount: 50 },
+  { id: 4, description: "Insurance", amount: 200 },
+];
+
+export default function ExpensesList() {
+  return (
+    <ul className="h-[300px] bg-white rounded mt-4 shadow-md">
+      {expenses.map((expense) => (
+        <li key={expense.id} className="flex items-center px-4 py-2 border-b">
+          <p>{expense.description}</p>
+          <p className="ml-auto font-bold mr-[15px]">${expense.amount}</p>
+          <button
+            onClick={async () => {}}
+            className="text-[10px] h-[20px] w-[20px] bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            X
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
