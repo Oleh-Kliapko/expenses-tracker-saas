9cf884dc81629b3157d29c5cb7bbf1e5a53dd089
Oleh Kliapko <goldclub1977@gmail.com>
2025-02-13 17:51:57 +0200

    ADD: prisma

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,4 +1,4 @@
-import { IExpense, IRoute } from "./interfaces";
+import { IRoute } from "./interfaces";
 
 export const routes: IRoute[] = [
   {
@@ -11,9 +11,9 @@ export const routes: IRoute[] = [
   },
 ];
 
-export const expenses: IExpense[] = [
-  { id: 1, description: "Rent", amount: 1000 },
-  { id: 2, description: "Groceries", amount: 100 },
-  { id: 3, description: "Gas", amount: 50 },
-  { id: 4, description: "Insurance", amount: 200 },
-];
+// export const expenses: IExpense[] = [
+//   { id: 1, description: "Rent", amount: 1000 },
+//   { id: 2, description: "Groceries", amount: 100 },
+//   { id: 3, description: "Gas", amount: 50 },
+//   { id: 4, description: "Insurance", amount: 200 },
+// ];
